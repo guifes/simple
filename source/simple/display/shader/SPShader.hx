5f4e4b752199336a20696cf16441380774369eaf
Guilherme Fernandes <guifes@gmail.com>
2021-12-17 22:46:13 -0300

    Refactoring SPShader

--- source/simple/display/shader/SPShader.hx
+++ source/simple/display/shader/SPShader.hx
@@ -1,7 +1,7 @@
 package simple.display.shader;
 
 import haxe.Exception;
-import openfl.display.Shader;
+import openfl.display.DisplayObjectShader;
 import openfl.utils.ByteArray;
 
 #if !openfl_debug
@@ -9,73 +9,8 @@ import openfl.utils.ByteArray;
 @:noDebug
 #end
 
-class SPShader extends Shader
+class SPShader extends DisplayObjectShader
 {
-    @:glVertexHeader("
-        attribute vec4 openfl_Position;
-		attribute vec2 openfl_TextureCoord;
-
-		varying vec2 openfl_TextureCoordv;
-
-		uniform mat4 openfl_Matrix;
-		uniform vec2 openfl_TextureSize;
-    ")
-
-	@:glVertexBody("
-        openfl_TextureCoordv = openfl_TextureCoord;
-
-		gl_Position = openfl_Matrix * openfl_Position;
-    ")
-
-	@:glVertexSource("
-        #pragma header
-
-		void main(void) {
-
-			#pragma body
-
-		}
-    ")
-	
-	@:glFragmentHeader("
-        varying vec2 openfl_TextureCoordv;
-
-		uniform sampler2D openfl_Texture;
-		uniform vec2 openfl_TextureSize;
-    ")
-
-	@:glFragmentBody("
-        gl_FragColor = texture2D (openfl_Texture, openfl_TextureCoordv);
-    ")
-
-#if emscripten
-
-	@:glFragmentSource("
-        #pragma header
-
-		void main(void) {
-
-			#pragma body
-
-			gl_FragColor = gl_FragColor.bgra;
-
-		}
-    ")
-
-#else
-
-	@:glFragmentSource("
-        #pragma header
-
-		void main(void) {
-
-			#pragma body
-
-		}
-    ")
-        
-#end
-
     public function new(code:ByteArray = null)
 	{
 		super(code);
